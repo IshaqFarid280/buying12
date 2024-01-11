ddb0317d76fdcb0ff5de61c118ff2011d27514ff
IshaqFarid280 <IshaqFarid280>
2024-01-11 14:39:30 +0500

    0.00012344555

---
+++ lib/widget/Custom_Text_feild.dart
@@ -0,0 +1,34 @@
+import 'package:admin_panel/consts/colors.dart';
+import 'package:flutter/material.dart';
+
+class CustomTextFeild extends StatelessWidget {
+  final TextEditingController controller ;
+  final String hintText ;
+   CustomTextFeild({required this.controller,required this.hintText});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      style:const  TextStyle(color: whiteColor),
+      cursorColor: buttonColors,
+      enabled: true,
+      controller: controller,
+      decoration: InputDecoration(
+        hintText: hintText,
+        hintStyle:const TextStyle(color: iconColor),
+        border: OutlineInputBorder(
+          borderSide:const BorderSide(color:iconColor),
+          borderRadius: BorderRadius.circular(10),
+        ),
+        enabledBorder:OutlineInputBorder(
+          borderSide:const BorderSide(color:iconColor,),
+          borderRadius: BorderRadius.circular(10),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderSide:const BorderSide(color:iconColor,width: 3),
+          borderRadius: BorderRadius.circular(10),
+        ),
+      ),
+    );
+  }
+}
